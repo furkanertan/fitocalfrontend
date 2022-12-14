0577a8b1d3995ecab2fd7dc76db81474ec546a7c
furkanertan <furkanertan23@gmail.com>
2022-12-14 22:20:09 +0100

    Topbar update

--- src/bars/Topbar.js
+++ src/bars/Topbar.js
@@ -1,12 +1,21 @@
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, IconButton, Toolbar } from "@material-ui/core";
 import React from "react";
+import { TopbarStyle } from "./TopbarStyle";
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Topbar() {
+  const classes = TopbarStyle();
   return (
     <div>
       <AppBar position ='fixed' elevation={1}>
-        <Toolbar>
+        <Toolbar className={classes.topbar}>
+          <IconButton>
+            <MenuIcon>
+            </MenuIcon>
+          </IconButton>
         </Toolbar>
+        <div>
+        </div>
       </AppBar>
     </div>
   );
